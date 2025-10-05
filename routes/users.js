d16bb9e40d0717df47d6c0f85fbb1bc2ba7f40bb
HamiLia <cvsd1144531@gmail.com>
2025-10-05 04:44:07 -0400

    lab 5

---
+++ routes/users.js
@@ -0,0 +1,54 @@
+const routerUser = express.Router();
+const userData = require('../user.json');
+const express = require('express')
+
+/*
+- Return all details from user.json file to client as JSON format
+*/
+router.get('/profile', (req,res) => {
+  res.json(userData);
+});
+
+
+
+/*
+- Modify /login router to accept username and password as JSON body parameter
+- Read data from user.json file
+- If username and  passsword is valid then send resonse as below 
+    {
+        status: true,
+        message: "User Is valid"
+    }
+- If username is invalid then send response as below 
+    {
+        status: false,
+        message: "User Name is invalid"
+    }
+- If passsword is invalid then send response as below 
+    {
+        status: false,
+        message: "Password is invalid"
+    }
+*/
+routerUser.post('/login', (req, res)=>{
+    const { username, password } = req.body;
+
+    if(usernae === userData.username) {
+        if(password === userData.password){
+            res.json({
+                status: true,
+                message: "User is valid"
+            });
+        }else {
+            res.json({
+                status: false,
+                message: "Password is invalid"
+            });
+        
+        } else {
+            res.json({
+                status: false,
+                message: "User Name is invalid"
+            });
+    }
+});
